c9516ae1c875887eeb79d5d764bdbfca9e6fc7f1
Anuj Mishra <100562544+Anuj-gr8@users.noreply.github.com>
2023-10-09 20:21:22 +0530

    Time: 1744 ms (43.78%), Space: 55.7 MB (56.76%) - LeetHub

---
+++ 2165-plates-between-candles/2165-plates-between-candles.py
@@ -0,0 +1,15 @@
+import bisect
+class Solution:
+    def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
+        plt_idx = [i for i in range(len(s)) if s[i]=='|']
+        ans = []
+        for it in queries:
+            l = it[0]
+            r = it[1]
+            i = bisect.bisect_left(plt_idx,l,0,len(plt_idx))
+            j = bisect.bisect_right(plt_idx,r,0,len(plt_idx))-1
+            if j<=i:
+                ans.append(0)
+            else:
+                ans.append(plt_idx[j]-plt_idx[i]-(j-i))
+        return ans
