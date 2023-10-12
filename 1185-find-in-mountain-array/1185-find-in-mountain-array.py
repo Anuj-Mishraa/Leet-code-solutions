01914e715a01c3dbcb7f54fe23b5f9734774cc50
Anuj Mishra <100562544+Anuj-gr8@users.noreply.github.com>
2023-10-12 10:56:09 +0530

    Time: 40 ms (46.36%), Space: 16.9 MB (98.91%) - LeetHub

---
+++ 1185-find-in-mountain-array/1185-find-in-mountain-array.py
@@ -0,0 +1,43 @@
+# class MountainArray:
+#     def get(self, index: int) -> int:
+#     def length(self) -> int:
+
+class Solution:
+    def findInMountainArray(self, target: int, mountain_arr: 'MountainArray') -> int:
+        n = mountain_arr.length()
+        
+        left, right = 0, n - 1
+        while left < right:
+            mid = left + (right - left) // 2
+            mid_val = mountain_arr.get(mid)
+            next_val = mountain_arr.get(mid + 1)
+            if mid_val < next_val:
+                left = mid + 1
+            else:
+                right = mid
+        
+        peak = left
+
+        left, right = 0, peak
+        while left <= right:
+            mid = left + (right - left) // 2
+            mid_val = mountain_arr.get(mid)
+            if mid_val == target:
+                return mid
+            elif mid_val < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        left, right = peak, n - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            mid_val = mountain_arr.get(mid)
+            if mid_val == target:
+                return mid
+            elif mid_val < target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        return -1
